3d6ab70e51d42ff10ed48748af3835ed5e0d2e12
balqozz <145304918+balqozz@users.noreply.github.com>
2025-04-23 17:35:59 +0700

    rev prac3.5

--- state-management-praktikum/src/app/redux_coba/page.js
+++ state-management-praktikum/src/app/redux_coba/page.js
@@ -1,5 +1,6 @@
-import ReduxProvider from '@providers/ReduxProvider';
-import ReduxCounter from '@components/ReduxCounter';
+import ReduxProvider from '../../providers/ReduxProvider';
+import ReduxCounter from '../../components/ReduxCounter';
+
 
 export default function Coba() {
   return (
