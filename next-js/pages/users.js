828f5bf503e76707269e26978102d9b2bff4c5f2
balqozz <145304918+balqozz@users.noreply.github.com>
2025-04-24 14:49:29 +0700

    tugas1

---
+++ next-js/pages/users.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
+
+export default function UsersPage({ users }) {
+  return (
+    <div>
+      <h1>Daftar Pengguna</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <a href={`/users/${user.id}`}>{user.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
