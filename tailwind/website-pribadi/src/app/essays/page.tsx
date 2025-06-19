5f4134fd1eb40f05955b663310c623cc46522369
balqozz <balqis1424@gmail.com>
2025-06-19 13:50:27 +0700

    prac3

--- tailwind/website-pribadi/src/app/essays/page.tsx
+++ tailwind/website-pribadi/src/app/essays/page.tsx
@@ -1,3 +1,21 @@
-export default function Essays() {
-  return <div>Esai</div>;
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Esai',
+  description: 'Halaman untuk menampilkan daftar artikel yang telah ditulis.',
+  openGraph: {
+    title: 'Esai',
+    description: 'Halaman untuk menampilkan daftar artikel yang telah ditulis.',
+  },
+};
+
+export default function Projects() {
+  return (
+    <div className="mt-16 px-8">
+      <header>
+        <h1 className="font-bold text-4xl text-zinc-800">Esai</h1>
+      </header>
+    </div>
+  );
 }
+   
--- tailwind/website-pribadi/src/app/projects/page.tsx
+++ tailwind/website-pribadi/src/app/projects/page.tsx
@@ -1,3 +1,21 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Proyek',
+  description: 'Halaman untuk menampilkan proyek-proyek yang telah diselesaikan.',
+  openGraph: {
+    title: 'Proyek',
+    description: 'Halaman untuk menampilkan proyek-proyek yang telah diselesaikan.',
+  },
+};
+
 export default function Projects() {
-  return <div>Proyek</div>;
+  return (
+    <div className="mt-16 px-8">
+      <header>
+        <h1 className="font-bold text-4xl text-zinc-800">Proyek</h1>
+      </header>
+    </div>
+  );
 }
+    
