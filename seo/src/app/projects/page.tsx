eb57d4544703aae47fdd3631d72028f11d0e1cf1
balqozz <balqis1424@gmail.com>
2025-06-22 23:38:52 +0700

    prac3

--- seo/src/app/projects/page.tsx
+++ seo/src/app/projects/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { defaultMetadata } from '../utils/metadata';
+import { defaultMetadata } from '../../utils/metadata';
 
 export const metadata: Metadata = {
   title: 'Tentang Saya',
@@ -14,7 +14,7 @@ export default function Home({ metadata }: any) {
   return (
     <div className="mt-16 px-8">
       <header>
-        <h1 className="font-bold text-4xl text-zinc-800">Tentang Saya</h1>
+        <h1 className="font-bold text-4xl text-zinc-800">Proyek</h1>
       </header>
     </div>
   );
